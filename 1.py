afa6c9a0e4d4a95f24383cb6b85271aa1fd50307
wenchao-h <wenchao_h@outlook.com>
2018-10-25 16:28:55 +0800

    1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+import os
+print(__file__)
+
+
+print(os.path.abspath(__file__))
+
+print(os.path.dirname(os.path.abspath(__file__)))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
